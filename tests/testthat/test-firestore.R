71606f651ecd94b323b8f19532b36ce3b7855770
Zack <jiz010@ucsd.edu>
2018-07-25 15:29:10 -0700

    Simple test, auth required

--- tests/testthat/test-firestore.R
+++ tests/testthat/test-firestore.R
@@ -2,7 +2,7 @@ library("testthat")
 library("fireData")
 
 test_that("Test Firestore list collection ids functionality", {
-  response <- listCollectionIds("gsoc2018-d05d8", "test/test", 1, token = code)
+  response <- listCollectionIds("gsoc2018-d05d8", "test/test", 1, token = "ya29.Glv8BZLGoUhDUdcB75R6vGmiNwxumWr1LONMNxxZrE_oxBB_ed8Slv2ubqpBq4euvYLC5poclOXAhELNg8Tulp_sGdQlWKOmblE637-bbnhPdQHd8_41xS3pHsdL")
   response <- httr::content(response, "parsed")
   expect_null(response$error)
 })
